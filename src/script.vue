16b830685263ea7018ba2ee9d3afa2487893b662
Helena Hollænder <helena.hollaender@gmail.com>
2025-01-13 14:37:19 +0100

    with_spheres_gui_yay

--- src/script.vue
+++ src/script.vue
@@ -4,7 +4,24 @@ import CANNON from 'cannon';
 import { ref } from 'vue';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as dat from 'dat.gui';
+import GUI from 'lil-gui';
+
+//Debug
+const gui = new GUI();
+const debugObject = {};
+
+debugObject.createSphere = () =>
+{
+    createSphere(Math.random() * 0.5, {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3
+    });
+}
+
+gui.add(debugObject, 'createSphere');
+
+
 
 
 //Canvas
@@ -13,13 +30,61 @@ const canvas = document.querySelector('canvas.webgl');
 //Scene
 const scene = new THREE.Scene();
 
-//Create sphere - mesh
-// const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-// const sphereMaterial = new THREE.MeshStandardMaterial({
-//     metalness: 0.3, 
-//     roughness: 0.4,
-//     color: 0xff0000
-// });
+//Physics world
+const world = new CANNON.World();
+world.broadphase = new CANNON.SAPBroadphase(world);
+world.allowSleep = true;
+world.gravity.set(0, -9.82, 0);
+
+//Physics deault material
+const defaultMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
+    friction: 0.1,
+    restitution: 0.7
+});
+world.addContactMaterial(defaultContactMaterial);
+
+//Floor physics
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
+
+const objectsToUpdate = [];
+
+//Create sphere (mesh and body)
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3, 
+    roughness: 0.4,
+    color: 0xff0000
+});
+
+const createSphere = (radius, position) => {
+    //THREE.js
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    mesh.scale.set(radius, radius, radius);
+    mesh.castShadow = true;
+    mesh.position.copy(position);
+    scene.add(mesh);
+
+    //Cannon.js body
+    const shape = new CANNON.Sphere(radius);
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape, 
+        material: defaultMaterial
+    });
+    body.position.copy(position);
+    world.addBody(body);
+    
+    //Save in objects array:
+    objectsToUpdate.push({mesh, body})
+}
+
 
 //Light
 const ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
@@ -32,8 +97,8 @@ const sizes = {
 }
 window.addEventListener('resize', () => {
     //Update sizes
-    sizes.width = 600;
-    sizes.height = 800;
+    // sizes.width = 600;
+    // sizes.height = 800;
 
     //Update camera
     camera.aspect = sizes.width / sizes.height;
@@ -71,6 +136,15 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime();
     const deltaTime = elapsedTime - oldElapsedTime;
     oldElapsedTime = elapsedTime;
+
+    //Update physics world
+    world.step(1 / 60, deltaTime, 3);
+
+    for(const objects of objectsToUpdate)
+    {
+        objects.mesh.position.copy(objects.body.position);
+        objects.mesh.quaternion.copy(objects.body.quaternion);
+    }
 
     //Update controls
     controls.update();
