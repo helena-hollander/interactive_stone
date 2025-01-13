849e9d39f6e4aa423e27eeaebed875d79380bd2c
Helena Hollænder <helena.hollaender@gmail.com>
2025-01-13 11:36:23 +0100

    interactive_stone_start

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import * as THREE from 'three';
+import { ref } from 'vue';
+import sceneScript from './script.vue';
+</script>
+
+<template>
+<sceneScript></sceneScript>
+
+</template>
+
---
+++ src/script.vue
@@ -0,0 +1,8 @@
+<script setup>
+import * as THREE from 'three';
+
+console.log(THREE); // THREE is available here
+</script>
+<template>
+
+</template>
